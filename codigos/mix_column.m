93d74a16c59b5e429f33462c4a4b8834a5a1bc93
rafaelcnunes2 <rafael.nunes3@hotmail.com>
2017-04-04 01:18:08 -0300

    Update mix_column.m

--- codigos/mix_column.m
+++ codigos/mix_column.m
@@ -2,12 +2,13 @@ function state = mix_column(A)
 %entrada A em decimal 4x4
 %saida state em decimal 4x4
 
+%matriz multiplicativa
 M = [2 3 1 1;
      1 2 3 1;
      1 1 2 3;
      3 1 1 2];
         
-        
+%matriz de entrada em decimal
 A = [212 212 212 212;
      191 191 191 191;
      93 93 93 93;
@@ -19,15 +20,16 @@ A = [212 212 212 212;
 aux = [];
 state = [];
 
+%3 fors para preencher uma matriz novamente após fazer a multiplicação e xor de cada linha por cada coluna
 for k = 1:length(M)
   for j = 1:length(M)
     for i = 1:length(M)
     A(i,j)
     M(j,i)
-    aux(i) = mul_t(A(i,j),M(j,i));
+    aux(i) = mul_t(A(i,j),M(j,i)); %utiliza da função que multiplica pela tabela
     end
     aux2 = bitxor(aux(1),aux(2));
     aux2 = bitxor(aux2,aux(3));
-    state(j,k) = bitxor(aux2,aux(4))
+    state(j,k) = bitxor(aux2,aux(4))  %faz o xor bit a bit de todos os aux e armazena em uma posição da nova matriz
   end
 end
